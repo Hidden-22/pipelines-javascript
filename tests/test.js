a069442abaac28f4b03e563ab17cd13e9ff9567d
Venish shiyani <36152545+Hidden-22@users.noreply.github.com>
2020-06-04 17:44:29 +0530

    update ts.js

--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 
 describe('String', function () {
     describe('#indexOf', function () {
+        
         it('should return -1 when the substring is not present', function () {
             var myString = 'test';
 
